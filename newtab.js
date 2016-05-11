7ebc20dc9bb383ba9e2f221a0e038ccf1da892f5
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-11 10:13:50 +0300

    Add key shortcuts

--- newtab.js
+++ newtab.js
@@ -49,6 +49,7 @@ rect = function() {
       }
     }
 
+    ctxOverlay.strokeStyle="#FF0000";
     ctxOverlay.strokeRect(x, y, w, h);
 
     selectionX = x;
@@ -87,7 +88,6 @@ let tool = new rect();
 let selectionX, selectionY, selectionW, selectionH = 0;
 let width = 0;
 let height = 0;
-ctxOverlay.strokeStyle="#FF0000";
 
 function fetchImage(url) {
   let imgTag = document.createElement('img');
@@ -137,7 +137,7 @@ function upload(base64Img) {
   img = '<html><head><title>Google Image</title></head><body><form id="f" method="POST" action="https://www.google.com/searchbyimage/upload" enctype="multipart/form-data"><input type="hidden" name="image_content" value="' + img + '"><input type="hidden" name="filename" value=""><input type="hidden" name="image_url" value=""><input type="hidden" name="sbisrc" value="cr_1_5_1"></form><script>document.getElementById("f").submit();</script></body></html>';
   let f = 'data:text/html;charset=utf-8;base64,' + window.btoa(img);
 
-  chrome.tabs.create({'url': f, 'selected': true});
+  chrome.tabs.create({'url': f, 'selected': false});
 }
 
 function google() {
@@ -162,6 +162,15 @@ function bindHeader() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  document.body.addEventListener('keypress', (e) => {
+    if (e.keyCode == 1081 ||
+      e.keyCode == 1049 ||
+      e.keyCode == 113 ||
+      e.keyCode == 81) {
+        google();
+    }
+  });
+
   bindHeader();
   initDrawer();
 });
